c5684d0eeaa9da4e656dffdf468db52d324f456b
xinjuan Deng <dengxinj@gmail.com>
2020-07-31 23:39:01 -0400

    the image couldnt flex

--- memeGenerator/memeGenerator.js
+++ memeGenerator/memeGenerator.js
@@ -16,17 +16,23 @@ image.style.width = "400px";
 image.style.height = "400px";
 
 
-let topEl = document.createElement('div');
+let topEl = document.createElement('h2');
 topEl.innerText = textTop.value;
 topEl.classList.add('top');
 
-let bottomEl = document.createElement('div');
+let bottomEl = document.createElement('h2');
 bottomEl.innerText = textBottom.value;
 bottomEl.classList.add('buttom');
 
-divEl.appendChild(image);
-divEl.appendChild(bottomEl);
-divEl.appendChild(topEl);
+let memeDiv = document.createElement('div');
+memeDiv.style.position = 'relative';
+memeDiv.style.width = "400px";
+memeDiv.style.height = "400px";
+
+memeDiv.appendChild(image);
+memeDiv.appendChild(bottomEl);
+memeDiv.appendChild(topEl);
+divEl.appendChild(memeDiv);
 
  }
 
@@ -34,6 +40,6 @@ submitButton.addEventListener('click',createImg);
 
 divEl.addEventListener('dblclick',function(e){
 
-    divEl.removeChild(e.target);
+    e.target.parentElement.remove();
 
 })
