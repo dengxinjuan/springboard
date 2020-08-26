3cc283524c3617fa70814cec4981e47d92f96e7a
xinjuan Deng <dengxinj@gmail.com>
2020-08-26 14:55:33 -0400

    fixed api-js

--- hack-or-snooze-ajax-api/api-classes.js
+++ hack-or-snooze-ajax-api/api-classes.js
@@ -10,57 +10,88 @@ class StoryList {
     this.stories = stories;
   }
 
+  static async getStories() {
+    // query the /stories endpoint (no auth required)
+    const response = await axios.get(`${BASE_URL}/stories`);
+    // turn the plain old story objects from the API into instances of the Story class
+    const stories = response.data.stories.map(story => new Story(story));
+
+    // build an instance of our own class using the new array of stories
+    const storyList = new StoryList(stories);
+    return storyList;
+
+  }
+
+  async addStory(user, newStory) {
+  
+    const response = await axios.post(`${BASE_URL}/stories`,{params:{story:newStory,token:user.loginToken}});
+     newStory= new Story(response.data.story);
+     this.stories.unshift(newStory);
+     user.ownStories.unshift(newStory);
+     return newStory;
+  }
+
+
+async removeStory(user, storyId){
+  await axios({
+    url: `${BASE_URL}/stories/${storyId}`,
+    method: "DELETE",
+    data: {
+      token: user.loginToken
+    },
+  });
+  this.stories = this.stories.filter(story => story.storyId !== storyId);
+
+  user.ownStories = user.ownStories.filter(s => s.storyId !== storyId
+  );
+}
+
+
+
+  async update(user, storyData) {
+    const response = await axios({
+      url: `${BASE_URL}/stories/${this.storyId}`,
+      method: "PATCH",
+      data: {
+        token: user.loginToken,
+        story: storyData
+      }
+    });
+
+    const { author, title, url, updatedAt } = response.data.story;
+
+    this.author = author;
+    this.title = title;
+    this.url = url;
+    this.updatedAt = updatedAt;
+
+    return this;
+  }
+}
+
+
+
+
+/**
+ * The User class to primarily represent the current user.
+ *  There are helper methods to signup (create), login, and getLoggedInUser
+ */
+
 class User {
   constructor(userObj) {
     this.username = userObj.username;
     this.name = userObj.name;
     this.createdAt = userObj.createdAt;
     this.updatedAt = userObj.updatedAt;
+
+    // these are all set to defaults, not passed in by the constructor
     this.loginToken = "";
     this.favorites = [];
     this.ownStories = [];
-
-}
-
-
-  async addStory(user, newStory) {
-    const response = await axios({
-      method: "POST",
-      url: `${BASE_URL}/stories`,
-      data: {
-        token: user.loginToken,
-        story: newStory,
-      }
-    });
-    newStory = new Story(response.data.story);
-    this.stories.unshift(newStory);
-    user.ownStories.unshift(newStory);
-
-    return newStory;
   }
 
 
-
-
-  async removeStory(user, storyId) {
-    await axios({
-      url: `${BASE_URL}/stories/${storyId}`,
-      method: "DELETE",
-      data: {
-        token: user.loginToken
-      },
-    });
-    this.stories = this.stories.filter(story => story.storyId !== storyId);
-    user.ownStories = user.ownStories.filter(s => s.storyId !== storyId
-    );
-  }
-
-
- 
-
-
-
-//SignUp
+  //signup
   static async create(username, password, name) {
     const response = await axios.post(`${BASE_URL}/signup`, {
       user: {
@@ -79,11 +110,7 @@ class User {
     return newUser;
   }
 
-  /* Login in user and return user instance.
-
-   * - username: an existing user's username
-   * - password: an existing user's password
-   */
+ //login in
 
   static async login(username, password) {
     const response = await axios.post(`${BASE_URL}/login`, {
@@ -134,18 +161,76 @@ class User {
     existingUser.ownStories = response.data.user.stories.map(s => new Story(s));
     return existingUser;
   }
+
+
+  //getUser details
+  async retrieveDetails() {
+    const response = await axios.get(`${BASE_URL}/users/${this.username}`, {
+      params: {
+        token: this.loginToken
+      }
+    });
+
+    this.name = response.data.user.name;
+    this.createdAt = response.data.user.createdAt;
+    this.updatedAt = response.data.user.updatedAt;
+
+    this.favorites = response.data.user.favorites.map(s => new Story(s));
+    this.ownStories = response.data.user.stories.map(s => new Story(s));
+
+    return this;
+  }
+
+//update User deatails
+async updateUser(userData){
+  const response= await axios.patch(`${BASE_URL}/users/${this.username}`,{params:{user:userData, token:this.loginToken}});
+  this.name = response.data.user.name;
+  this.password =response.data.user.password;
+    return this;
+}
+//delete user
+async deteleUser(){
+    await axios.delete(`${BASE_URL}/users/${this.username}`,{params: {
+    token: this.loginToken}})
 }
 
-/**
- * Class to represent a single story.
- */
+//add favorite
+async addFavorite(story) {
+  this.favorites.push(story);
+  const token=this.loginToken;
+  await axios.post(`${BASE_URL}/users/${this.username}/favorites/${story.storyId}`,{
+    "token":token
+  })
+}
+//removes favorite from user favorite list
+async removeFavorite(story) {
+
+  this.favorites = this.favorites.filter(s => s.storyId !== story.storyId);
+  await axios({
+    url: `${BASE_URL}/users/${this.username}/favorites/${story.storyId}`,
+    method: "DELETE",
+    data: {
+      token: this.loginToken
+    },
+  });
+}
+//to check the story is favorite
+isFavorite(story) {
+  return this.favorites.some(s => (s.storyId === story.storyId));
+}
+
+}
+   
+
+
+
+
+
+
+
 
 class Story {
 
-  /**
-   * The constructor is designed to take an object for better readability / flexibility
-   * - storyObj: an object that has story properties in it
-   */
 
   constructor(storyObj) {
     this.author = storyObj.author;
@@ -156,26 +241,27 @@ class Story {
     this.createdAt = storyObj.createdAt;
     this.updatedAt = storyObj.updatedAt;
   }
+
+  async update(user, storyData) {
+    const response = await axios({
+      url: `${BASE_URL}/stories/${this.storyId}`,
+      method: "PATCH",
+      data: {
+        token: user.loginToken,
+        story: storyData
+      }
+    });
+
+    const { author, title, url, updatedAt } = response.data.story;
+
+    this.author = author;
+    this.title = title;
+    this.url = url;
+    this.updatedAt = updatedAt;
+
+    return this;
+  }
 }
 
 
-async update(user, storyData) {
-  const response = await axios({
-    url: `${BASE_URL}/stories/${this.storyId}`,
-    method: "PATCH",
-    data: {
-      token: user.loginToken,
-      story: storyData
-    }
-  });
 
-  const { author, title, url, updatedAt } = response.data.story;
-
-  this.author = author;
-  this.title = title;
-  this.url = url;
-  this.updatedAt = updatedAt;
-
-  return this;
-}
-};
