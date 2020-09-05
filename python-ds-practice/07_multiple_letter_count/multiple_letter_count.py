8a0fbff43aa43abf0429e3f91fced8ae28a3cae7
xinjuan Deng <dengxinj@gmail.com>
2020-09-04 23:22:30 -0400

    python-ds-practice

---
+++ python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,15 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    dic ={}
+    for l in phrase:
+        dic[l] = dic.get(l,0)+1
+    return dic
+
+    
---
+++ python-ds-practice/26_vowel_count/vowel_count.py
@@ -0,0 +1,16 @@
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+    phrase = phrase.lower()
+    dic ={}
+    for letter in phrase:
+        if letter in "aeiou":
+            dic[letter]= dic.get(letter,0)+1
+    return dic
+    
